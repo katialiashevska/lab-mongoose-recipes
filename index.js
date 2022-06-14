fdf5e93002c25b1a3cdcd0f23cd579e60d829f3e
Katia <kate.liashevska@gmail.com>
2022-06-14 19:32:46 +0200

    lab done

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+// Iteration 2
+const vareniki = {
+  title: "Vareniki",
+  level: "Amateur Chef",
+  ingredients: [
+    "flour", 
+    "salt", 
+    "vegetable oil", 
+    "potatoes", 
+    "mushrooms", 
+    "onions", 
+    "parsley"
+  ],
+  cuisine: "Ukrainian",
+  dishType: "main_course",
+  image: "https://petersfoodadventures.com/wp-content/uploads/2018/09/vareniki.jpg",
+  duration: 45,
+  creator: "Katia",
+}
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
@@ -15,9 +35,50 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+
+  // Iteration 2
+  .then(async() => {
+    await Recipe.create(vareniki)
+      .then((recipe) => console.log(`"${title}" added to the database`))
+      .catch(error => console.log(error));
   })
   .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+    console.error(error);
+  })
+
+  // Iteration 3
+  .then(async() => {
+    await Recipe.insertMany(data)
+      .then(list => {
+        list.forEach(recipe => console.log(`"${recipe.title}" added to the database`))
+      })
+      .catch(error => console.log(error))
+  })
+
+  // Iteration 4
+  .then(async() => {
+    await Recipe.findOneAndUpdate({
+      title: "Rigatoni alla Genovese"
+      },
+      {
+        duration: 100
+      })
+      .then(() => console.log("Update successful"))
+      .catch(error => console.log(error))
+  })
+
+  // Iteration 5
+  .then(async() => {
+    await Recipe.deleteOne({
+      title: "Carrot Cake",
+      })
+      .then(() => console.log("Delete successful"))
+      .catch(error => console.log(error))
+  })
+
+  // Iteration 6
+  .then(async() => {
+    await mongoose.disconnect(MONGODB_URI)
+      .then(console.log(`Disconnected from the database, ${mongoose.connection.readyState}`))
+      .catch(error => console.log("An error happened while disconnecting", error));
+      })
